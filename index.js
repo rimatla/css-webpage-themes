8db361ca9a6917373855b7e9a0808cbf86f5beb8
rimatla <alt.coelho@gmail.com>
2020-10-08 22:11:55 -0400

    add selects

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
-// get the inputs
-const inputs = [].slice.call(document.querySelectorAll('.controls input'))
+const options = document.querySelector('#colors-dropdown').addEventListener('change', function() {
+  let greet = document.querySelector('.greet')
+  let color = document.documentElement.style
+  // input  changes
+  if (this.value === 'BARCELONA') {
+    color.setProperty('--primary', '#004D98')
+    color.setProperty('--secondary', '#A50044')
+    greet.innerHTML = 'Benvingut!'
+  }
 
-// listen for changes
-inputs.forEach((input) => input.addEventListener('change', handleUpdate))
+  if (this.value === 'CITY') {
+    color.setProperty('--primary', '#6caddf')
+    color.setProperty('--secondary', '#FFFFFF')
+    greet.innerHTML = 'Welcome!'
+  }
 
-function handleUpdate(e) {
-  const suffix = this.id === 'base' || this.id === 'base2' ? '' : 'px'
+  if (this.value === 'BORUSSIA') {
+    color.setProperty('--primary', '#FDE100')
+    color.setProperty('--secondary', '#000000')
+    greet.innerHTML = 'Welcome!'
+  }
 
-  document.documentElement.style.setProperty(`--${this.id}`, this.value + suffix)
-}
+  if (this.value === 'MADRID') {
+    color.setProperty('--primary', '#00519e')
+    color.setProperty('--secondary', '#FFFFFF')
+    greet.innerHTML = 'Bienvenido!'
+  }
+
+  if (this.value === 'PALMEIRAS') {
+    color.setProperty('--primary', '#1e5b3c')
+    color.setProperty('--secondary', '#FFFFFF')
+    greet.innerHTML = 'Seja Bem Vindo!'
+  }
+
+  if (this.value === 'INTER') {
+    color.setProperty('--primary', '#1063a6')
+    color.setProperty('--secondary', '#040707')
+    greet.innerHTML = 'Benvenuto!'
+  }
+
+  if (this.value === 'GUAN') {
+    color.setProperty('--primary', '#930111')
+    color.setProperty('--secondary', '#efae26')
+    greet.innerHTML = 'Huānyíng!'
+  }
+
+  if (this.value === 'PSG') {
+    color.setProperty('--primary', '#1a2b4c')
+    color.setProperty('--secondary', '#c7201a')
+    greet.innerHTML = 'Bienvenu!'
+  }
+
+  if (this.value === 'BENFICA') {
+    color.setProperty('--primary', '#c00001')
+    color.setProperty('--secondary', '#FFFFFF')
+    greet.innerHTML = 'Bem Vindo!'
+  }
+})
